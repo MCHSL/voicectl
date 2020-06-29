f8a3f9e6e7cba250bc8461b989207d2760cc7f51
1fbff5f83b23d39d38b1dfcb4cac8d9b <0a02d0db@opayq.com>
2020-06-30 00:42:48 +0200

    some fixes

--- voicectl.py
+++ voicectl.py
@@ -15,7 +15,7 @@ class CommandEntry:
 		self.__pattern = pattern
 		self.__varnames = {}
 		self.__can_chain = False
-		
+
 		re_pattern = []
 		words = pattern.split()
 		words_to_skip = 0
@@ -32,7 +32,7 @@ class CommandEntry:
 					word = word[:opening_loc]
 				elif word.endswith("..."):
 					word = word[:-3]
-					re_pattern.append(r"(.*)")
+					re_pattern.append(r"(.*?)")
 				else:
 					re_pattern.append(r"(\w+)")
 				self.__varnames[word] = current_group
@@ -52,18 +52,18 @@ class CommandEntry:
 				re_pattern.append(word)
 			else:
 				re_pattern.append(word)
-				
+
 		if re_pattern[-1] != "(.*)":
 			self.__can_chain = True
 			re_pattern.append("?(?:and then(?P<next_command>.*))?")
-			
+
 		print(re_pattern)
-			
+
 		self.__regex = re.compile(" ".join(re_pattern), re.IGNORECASE)
-		
+
 	def match(self, expr):
 		return self.__regex.fullmatch(expr)
-		
+
 	def create_kwargs(self, expr):
 		match = self.match(expr)
 		if match is None:
@@ -72,7 +72,7 @@ class CommandEntry:
 		result = {}
 		for varname in self.__varnames:
 			result[varname] = groups[self.__varnames[varname]]
-			
+
 		if self.__can_chain:
 			nextc = match.group("next_command")
 			if nextc:
@@ -82,47 +82,56 @@ class CommandEntry:
 		else:
 			return result, None
 
-		
 	def try_invoke(self, expr):
 		kwargs, next_command = self.create_kwargs(expr)
 		if kwargs is None:
 			return False, None
-		
+
 		self.__callback(**kwargs)
 		return True, next_command
-		
+
 
 class VoiceController:
-	def __init__(self, api_key, keyword="computer", region="westus", languages=None):
+	def __init__(self,
+	             api_key,
+	             keyword="computer",
+	             region="westus",
+	             languages=None):
 		self.__api_key = api_key
 		self.__keyword = keyword
 		self.__region = region
 		self.__languages = languages
 		if not self.__languages:
 			self.__languages = ["en-US"]
-			
-		self.__config = speechsdk.SpeechConfig(subscription=self.__api_key, region=self.__region)
-		if len(self.__languages) == 1:
-			self.__hq_recognizer = speechsdk.SpeechRecognizer(speech_config=self.__config, language=self.__languages[0])
-		else:
-			self.__hq_recognizer = speechsdk.SpeechRecognizer(speech_config=self.__config, auto_detect_source_language_config=speechsdk.languageconfig.AutoDetectSourceLanguageConfig(languages=self.__languages))
 
-		self.__lq_recognizer = KaldiRecognizer(Model("model"), 16000, self.__keyword + " [unk]")
-		
+		self.__config = speechsdk.SpeechConfig(subscription=self.__api_key,
+		                                       region=self.__region)
+		if len(self.__languages) == 1:
+			self.__hq_recognizer = speechsdk.SpeechRecognizer(
+			    speech_config=self.__config, language=self.__languages[0])
+		else:
+			self.__hq_recognizer = speechsdk.SpeechRecognizer(
+			    speech_config=self.__config,
+			    auto_detect_source_language_config=speechsdk.languageconfig.
+			    AutoDetectSourceLanguageConfig(languages=self.__languages))
+
+		self.__lq_recognizer = KaldiRecognizer(Model("model"), 16000,
+		                                       self.__keyword + " [unk]")
+
 		self.on_ready = lambda *x: x
 		self.on_triggered = lambda *x: x
 		self.on_begin_command = lambda *x: x
 		self.on_finish_command = lambda *x: x
 		self.on_unknown_command = lambda *x: x
 		self.on_error = lambda *x: x
-		
+
 		self.__commands = []
 		self.next_activation = time.time()
 		self.__active = False
-		
+
 	def add_command(self, pattern, callback):
 		self.__commands.append(CommandEntry(pattern, callback))
-		
+
 	def perform_all_commands(self, cmd):
 		while True:
 			has_match = False
@@ -138,20 +147,24 @@ class VoiceController:
 			if not has_match:
 				break
 		self.on_unknown_command(cmd)
-		
+
 	def listen_for_command(self):
 		self.__active = True
 		self.on_triggered()
-		speech = self.__hq_recognizer.recognize_once().text.translate(str.maketrans('', '', string.punctuation)).lower()
+		speech = self.__hq_recognizer.recognize_once().text.translate(
+		    str.maketrans('', '', string.punctuation)).lower()
 
 		print(speech)
 		self.perform_all_commands(speech)
 		self.__active = False
 
-
 	def start_listening(self):
 		p = pyaudio.PyAudio()
-		stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+		stream = p.open(format=pyaudio.paInt16,
+		                channels=1,
+		                rate=16000,
+		                input=True,
+		                frames_per_buffer=8000)
 		stream.start_stream()
 		self.on_ready()
 		while True:
@@ -160,7 +173,8 @@ class VoiceController:
 				break
 			if self.__lq_recognizer.AcceptWaveform(data):
 				result = self.__lq_recognizer.Result()
-				if not self.__active and json.loads(result)["text"] == self.__keyword:
+				jres = json.loads(result)
+				if not self.__active and jres["text"] == self.__keyword:
 					if self.next_activation > time.time():
 						continue
 					self.__active = True
